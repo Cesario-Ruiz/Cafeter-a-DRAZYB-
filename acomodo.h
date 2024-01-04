e6bed325771701ba71527994208872f8725e5ae8
César Ruíz <150985370+Cesario-Ruiz@users.noreply.github.com>
2024-01-03 18:49:58 -0600

    Delete acomodo.h

--- acomodo.h
+++
@@ -1,9 +0,0 @@
-/*
-    Encabezado que contiene la funcion gotoxy() -> Simulando las librerias <windows.h>/<conio.h>
-*/
-#include <stdio.h>
-
-void gotoxy(int x, int y)   // Las variables x & y reprenstan las coordenadas de la consola/terminal
-{                                           
-    printf("%c[%d;%df", 0x1B, y, x);        
-}
